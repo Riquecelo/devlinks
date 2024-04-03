20b07a9d5e2d5a951cf16011bf042609fabb65cb
Marcelo Santos <marcelo.has@hotmail.com>
2024-04-03 20:26:55 -0300

    adção de lógica para o botão

---
+++ script.js
@@ -0,0 +1,15 @@
+function toogleMode(){
+    const body = document.querySelector('body')
+    body.classList.toggle("light")
+    
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    /* substituir a imagem
+    if (body.classList.contains("light")) {
+
+
+    } else {
+
+    }*/
+}
